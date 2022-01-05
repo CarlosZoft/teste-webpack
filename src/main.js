afcfa1585a411ab0dc80f31f9523b8d4b3b13d1b
CarlosZoft <carlosoftwar@gmail.com>
2022-01-05 17:01:20 -0300

    refactor: trying imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { LButton } from 'layers-design-system'
+import Vue from "vue";
+import App from "./App.vue";
+//import layers from "layers-design-system";
+//Vue.use(layers);
+import { LButton } from "layers-design-system";
 
+Vue.component(LButton.name, LButton.component)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
+  
